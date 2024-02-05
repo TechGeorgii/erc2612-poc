a3b8ad6088c97e06a25c19eb829aff672fb2a8bf
TechGeorgii <master.wsd064@gmail.com>
2024-02-05 15:46:03 +0700

    ethers simple contract call

--- index.ts
+++ index.ts
@@ -1,7 +1,27 @@
-import { ethers, randomBytes } from "ethers";
+import { Wallet, ethers } from "ethers";
+import { config } from "dotenv";
 
 async function main() {
-  console.log("b");
+  config();
+
+  const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
+  //const wallet = new Wallet(process.env.PK!, provider);
+
+  const abi = [
+    "function decimals() view returns (string)",
+    "function symbol() view returns (string)",
+    "function balanceOf(address addr) view returns (uint)",
+  ];
+
+  const usdtContract = new ethers.Contract(
+    "0xc2132D05D31c914a87C6611C10748AEb04B58e8F", // Polygon mainnet
+    abi,
+    provider
+  );
+
+  const bal = await usdtContract.balanceOf("0x....");
+
+  console.log(bal);
 }
 
 main().catch((error) => {
